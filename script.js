82bd9ee8c0130aae02281f5575c76563ef501fc7
Lautaro <lautaro.reinoso999@gmail.com>
2023-06-26 20:00:13 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,3 @@
+ let button=document.getElementsByClassName("button-menu-toggle")
+ document.querySelector("button.button-menu-toggle").addEventListener("click",function()
+ { document.querySelector(".nav-links").classList.toggle("nav-links-responsive")})
